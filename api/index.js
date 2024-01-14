a0ff0d637c368bd305cefae30d85637b84a05d67
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-15 00:30:27 +0300

    connect to db and hide env in the gnore

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("MongoDB Connected");
+});
 const app = express();
 
 app.listen(3000, () => {

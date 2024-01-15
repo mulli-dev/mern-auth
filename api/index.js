30acda3a2944600e405dad08590bd617b1e57ff3
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-15 15:55:53 +0300

    create api test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -12,3 +13,14 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+/*app.get("/", (req, res) => {
+  res.json({
+    message: "Api is working!",
+  });
+});
+*/
+
+//above code with app stands for instance/object/class for Express Application a framework for Node.js
+
+app.use("/api/user", userRoutes);

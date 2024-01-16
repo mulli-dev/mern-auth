cf98328c4d08c9625f9530d2464a215233988a3e
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-16 14:44:48 +0300

    complete sign up page functionality with details

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,80 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        return;
+      }
+      setError(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  /* In summary, this handleSubmit function is typically associated with a form
+ submission. It prevents the default form submission, sends a POST request 
+ to a server endpoint using the fetch function, awaits the server response, 
+parses the response JSON, and logs the data to the console.   */
+
+  //console.log(formData); insted add onsubmit listener to the form
+
   return (
     <div className="max-w-lg p-3 mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
-          className="p-3 bg-slate-100"
+          className="p-3 rounded-lg bg-slate-100 "
+          onChange={handleChange} //this is an event listener so we add function at the top
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
-          className="p-3 bg-slate-100"
+          className="p-3 rounded-lg bg-slate-100"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
-          className="p-3 bg-slate-100"
+          className="p-3 rounded-lg bg-slate-100"
+          onChange={handleChange}
         />
 
-        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
-          {" "}
-          Sign up
+        <button
+          disabled={loading}
+          className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80"
+        >
+          {""}
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -37,6 +84,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="mt-5 text-red-700">{error && "Something went wrong!"}</p>
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+    //In web development, a proxy can refer to a server that sits between a client (e.g., a web browser) and a backend server.
+  },
+});

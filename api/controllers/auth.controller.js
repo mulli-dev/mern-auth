dd44fdbde69cb772f29aa3ecb877963bb720704f
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-16 16:27:40 +0300

    create signin api route with cookies

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -16,3 +17,23 @@ export const signup = async (req, res, next) => {
 };
 
 //but someone is able to see the password so what can I do
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const ValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!ValidPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc; // added to stop send password to user
+    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      //.json(validUser);
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};

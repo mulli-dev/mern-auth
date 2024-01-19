19c827193abd6dd18e1568bf9ca6c8e82608bb9d
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-19 19:22:43 +0300

    create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,40 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({
     message: "API is working!",
   });
+};
+
+// update user
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    //return res.status(401).json("You can only update your account!");
+    return next(errorHandler(401, "You can only update your account!"));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 // you're telling your Express app to apply this middleware to all incoming requests, so that if the request has a JSON body, it will be parsed into a JavaScript object that you can work with in your route handlers.
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
@@ -40,4 +43,5 @@ app.use((err, req, res, next) => {
     message,
     statusCode,
   });
+  next(error);
 });

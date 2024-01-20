b2e266f723f14b9436c7d672d65ebecbf8639754
Jacob Mulli <jacobmullie2@gmail.com>
2024-01-20 20:43:41 +0300

    adding another preproduction code

--- api/index.js
+++ api/index.js
@@ -16,11 +16,11 @@ mongoose
     console.log(err);
   });
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirnamedirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname), "client", "dist", "index.html");
